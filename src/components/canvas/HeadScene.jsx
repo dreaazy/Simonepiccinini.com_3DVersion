5544c74c92acfef0eecdf658ec2599f19b15271a
Simone <piccinini.simone2005@gmail.com>
2024-05-31 20:14:17 +0200

    head finished

--- src/components/canvas/HeadScene.jsx
+++ src/components/canvas/HeadScene.jsx
@@ -3,79 +3,77 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-
 const Head = ({ isMobile }) => {
-    const computer = useGLTF("./HeadNew/Head.gltf");
+  const computer = useGLTF("/headFinal/untitled.gltf");
 
-    return (
-        <mesh>
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.2} groundColor="lightblue" />
+      <spotLight
+        position={[-30, 50, 30]}
+        angle={0.9}
+        penumbra={1}
+        intensity={4}
+        castShadow
+        shadow-mapSize={2048}
+      />
+      <pointLight position={[4, 0, 0]} intensity={7} color="softwhite" />
 
-            <hemisphereLight intensity={0.1} groundColor="black" />
-            <spotLight
-                position={[-20, 60, 20]} // Changed position to move it further away
-                angle={0.5} // Increased angle to widen the light spread
-                penumbra={0.5} // Reduced penumbra to soften the edge
-                intensity={5} // Reduced intensity to make it less harsh
-                castShadow
-                shadow-mapSize={1024}
-            />
-            <pointLight position={[-1, -1, 3]} intensity={5.5} /> 
-
-            <primitive
-                object={computer.scene}
-                scale={isMobile ? 1.7 : 2}
-                position={isMobile ? [0, 0, -2.2] : [4, 0, 0]}
-                rotation={[0, 0, 0]}
-            />
-        </mesh>
-    );
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 1.7 : 3.5}
+        position={isMobile ? [0, 0, -2.2] : [0, 0, 0]}
+        rotation={[0, 0, 0]}
+      />
+    </mesh>
+  );
 };
 
 const HeadScene = () => {
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        // Add a listener for changes to the screen size
-        const mediaQuery = window.matchMedia("(max-width: 500px)");
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-        // Set the initial value of the `isMobile` state variable
-        setIsMobile(mediaQuery.matches);
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
 
-        // Define a callback function to handle changes to the media query
-        const handleMediaQueryChange = (event) => {
-            setIsMobile(event.matches);
-        };
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-        // Add the callback function as a listener for changes to the media query
-        mediaQuery.addEventListener("change", handleMediaQueryChange);
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-        // Remove the listener when the component is unmounted
-        return () => {
-            mediaQuery.removeEventListener("change", handleMediaQueryChange);
-        };
-    }, []);
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
-    return (
-        <Canvas
-            frameloop='demand'
-            shadows
-            dpr={[1, 2]}
-            camera={{ position: [20, 3, 7], fov: 25 }}
-            gl={{ preserveDrawingBuffer: true }}
-        >
-            <Suspense fallback={<CanvasLoader />}>
-                <OrbitControls
-                    enableZoom={false}
-                    enablePan={false}
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
-                />
-                <Head isMobile={isMobile} />
-            </Suspense>
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 7], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Head isMobile={isMobile} />
+      </Suspense>
 
-            <Preload all />
-        </Canvas>
-    );
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default HeadScene;
